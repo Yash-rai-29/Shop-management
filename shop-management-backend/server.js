342784bd545ba60113611fc1b360473bdc243985
Yash Rai <yash.rai@wehealapp.in>
2024-06-22 13:16:31 +0530

    update backend add vercel json

--- shop-management-backend/server.js
+++ shop-management-backend/server.js
@@ -16,6 +16,8 @@ app.use('/api/stocks', require('./routes/stocks'));
 app.use('/api/records', require('./routes/records'));
 app.use('/api/eventRecords', require('./routes/eventRecords'));
 app.use('/api/billHistory', require('./routes/billHistory'));
-
+app.get('/', (req, res) => {
+    res.send('Hello from Vercel!');
+  });
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
